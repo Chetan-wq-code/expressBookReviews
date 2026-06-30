3689441fe49c7dd813539fb30c520ce49c1a8f61
Chetan-wq-code <ckdevalatkar@gmail.com>
2026-06-30 17:30:36 +0530

    upto task3
    Implemented routes to get book list, details by ISBN, and by author.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,20 +13,47 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn
+  if(isbn){
+    res.send(books[isbn])
+  }
+
+    
+
+
+  return res.status(400).json({message: "Book not found"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  let author = req.params.author
+  let bookKeys = Object.keys(books)
+  const matchedKeys = bookKeys.filter(key =>{
+    const book = books[key]
+    return book.author.toLowerCase() === author.toLowerCase()
+  })
+
+  const matchedBooks = matchedKeys.map(key=>{
+    return {id:key, ...books[key]}
+  })
+
+  if(matchedBooks.length > 0){
+    res.send(matchedBooks)
+  }else{
+    res.status(402).json({message:"Book not found"})
+  }
+  
+    
+
+    
+  });
+  
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
