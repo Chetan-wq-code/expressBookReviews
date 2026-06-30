b4542adc0aea35ec78c887037d4155da583ab33e
Chetan-wq-code <ckdevalatkar@gmail.com>
2026-06-30 21:23:28 +0530

    upto task6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,17 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.body.username
+  let password = req.body.password
+  if(username && password){
+    if(!(users.some(user=> user.username === username))){
+        users.push({"username":username, "password":password})
+        return res.status(200).json({message:"User successfully registered. now you can login"})
+    }else{
+        return res.status(404).json({message:"User already exist"})
+    }
+  }
+  return res.status(404).json({message: "Unable to register user"});
 });
 
 // Get the book list available in the shop
@@ -57,14 +66,32 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title
+  const matchedBooks = []
+  for(const [key, book] of Object.entries(books)){
+    if(book.title.toLowerCase() === title.toLowerCase()){
+        matchedBooks.push({key, ...book})
+    }
+  }
+  if(matchedBooks.length>0){
+    return res.send(matchedBooks)
+  }else{
+    return res.status(402).json({message: "Book not found"});
+  }
+  
+  
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn
+  if(isbn){
+
+    return res.send(books[isbn].reviews)
+  }else{
+    return res.status(300).json({message: "Book Not found"});
+  }
+  
 });
 
 module.exports.general = public_users;
